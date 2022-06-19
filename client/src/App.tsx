34cbacc5066372e1e3eac8991cbd8b75deeb9f28
Baleksas <alexasbag17@gmail.com>
2022-06-19 21:22:48 +0100

    feat: client initial components created, prototype of routing created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Navigate,
+  Routes,
+} from "react-router-dom";
 
+import Auth from "./pages/Auth";
+import Main from "./pages/Main";
+import Events from "./pages/Events";
 function App() {
-  return <div className="App">Something</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/events" element={(<Events />) as any}></Route>
+        <Route path="/auth" element={(<Auth />) as any}></Route>
+        <Route path="/" element={(<Main />) as any}></Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
