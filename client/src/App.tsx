494c7406ee961fdb071671d9adc454d0933f2d39
Baleksas <alexasbag17@gmail.com>
2022-06-20 11:10:32 +0100

    feat: basic front end app bar implemented and pages for different purposes created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 import {
   BrowserRouter as Router,
   Route,
-  Link,
+  Link as RouterLink,
   Navigate,
   Routes,
 } from "react-router-dom";
@@ -11,15 +11,104 @@ import {
 import Auth from "./pages/Auth";
 import Main from "./pages/Main";
 import Events from "./pages/Events";
+import {
+  AppBar,
+  Toolbar,
+  MenuItem,
+  Link,
+  Box,
+  Container,
+  Typography,
+  Menu,
+  IconButton,
+  Avatar,
+  Tooltip,
+} from "@mui/material";
+import Bookings from "./pages/Bookings";
+const pages = ["Main", "Auth", "Events", "Bookings"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 function App() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <Router>
-      <Routes>
-        <Route path="/events" element={(<Events />) as any}></Route>
-        <Route path="/auth" element={(<Auth />) as any}></Route>
-        <Route path="/" element={(<Main />) as any}></Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <AppBar position="static">
+        <Container sx={{ width: 1 }}>
+          <Toolbar disableGutters>
+            <Box sx={{ display: "flex", flexGrow: 1 }}>
+              {pages.map((page) => (
+                <Box m={1}>
+                  <Button
+                    variant="contained"
+                    component={RouterLink}
+                    to={`${page.toLowerCase()}`}
+                  >
+                    {page}
+                  </Button>
+                </Box>
+              ))}
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box>
+        <Routes>
+          <Route path="/events" element={(<Events />) as any}></Route>
+          <Route path="/bookings" element={(<Bookings />) as any}></Route>
+          <Route path="/auth" element={(<Auth />) as any}></Route>
+          <Route path="/" element={(<Main />) as any}></Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Box>
     </Router>
   );
 }
