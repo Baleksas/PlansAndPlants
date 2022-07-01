48d4d1cb29619d8ba11a4122283ea7361bd2b126
Baleksas <alexasbag17@gmail.com>
2022-07-01 10:58:50 +0100

    feat: finish working example of dispatching in counter example

--- client/src/components/Counter.tsx
+++ client/src/components/Counter.tsx
@@ -1,14 +1,16 @@
 import { Button, Input, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "../features/counterSlice";
 import { RootState } from "../utils/reduxStore";
 
 const Counter = () => {
   const { value } = useSelector((state: RootState) => state.counter);
   const dispatch = useDispatch();
   const [amount, setAmount] = useState(0);
+
 
   return (
     <Box sx={{ mx: "auto" }} mt={3} width={1 / 3}>
@@ -25,12 +27,12 @@ const Counter = () => {
           flexDirection: "column",
         }}
       >
-        <Button variant="contained">Increment</Button>
-        <Button color="warning" variant="contained">
+        <Button variant="contained" onClick={()=> dispatch(increment())}>Increment</Button>
+        <Button color="warning" variant="contained" onClick={()=> dispatch(decrement())}>
           Decrement
         </Button>
         <Box  width={1} sx={{ display: "flex", mx:"auto", justifyContent: 'center' }}>
-          <Button sx={{ flexGrow: 1 }} variant="contained">Increment by :</Button>
+          <Button sx={{ flexGrow: 1 }} variant="contained" onClick={()=> dispatch(incrementByAmount(amount))}>Increment by :</Button>
           <Input sx={{ml: 1, pl:1, mt:1}}
             type="number"
             onChange={(e) => setAmount(parseInt(e.target.value))}
